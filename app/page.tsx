e421ed6ac27e09c48f83fa3c8a3db0e260e3000b
Wilfred Chen <wilfred@gmail.com>
2023-05-22 13:47:53 +1000

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 
+const ineter = Inter({ subsets: ["latin"] });
+
 export default function Home() {
-    return <main className={styles.main}></main>;
+    return <main></main>;
 }
